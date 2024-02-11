d415b23d7077999692acc99adbc8de2d7d487d30
Oksana <oxanachyrkova9@gmail.com>
2024-02-11 18:22:28 +0200

    contact form

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
+import os
+from dotenv import load_dotenv
+import smtplib
+
+load_dotenv("C:\\Users\\Oksana\\Desktop\\passwords.env.txt")
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 app = Flask(__name__)
 
@@ -33,6 +41,26 @@ def show_post(post_id):
         if blog_post["id"] == post_id:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        data = request.form
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=MY_EMAIL,
+                                msg=f"Subject:Contact form!!!\n"
+                                    f"\nName:{data['name']}"
+                                    f"\nEmail:{data['email']}"
+                                    f"\nPhone:{data['phone']}"
+                                    f"\nMessage:{data['message']}")
+
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
